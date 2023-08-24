797c9c8602014f8f138a84cbdd405f4fc71b6725
Miloš Jovanović <milos.jov.rs@gmail.com>
2023-08-24 09:15:52 +0200

    chore: Add React and Vue examples

---
+++ frameworks/react/post.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Post(props) {
+	return (
+		<article>
+			<Title>{props.title}</Title>
+
+			<Content>{props.children}</Content>
+
+			<Footer>Published on {props.date}</Footer>
+		</article>
+	);
+}
+
+let Title = styled.h1`
+	margin-bottom: 24px;
+`;
+
+let Content = styled.div`
+	margin-bottom: 24px;
+
+	p:not(:last-child) {
+		margin-bottom: 8px;
+	}
+`;
+
+let Footer = styled.footer`
+	border-top: 1px solid #000;
+	padding-top: 24px;
+`;
