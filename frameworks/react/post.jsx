3d5adc1eba560669a21720c70d88c73a7a4251be
Miloš Jovanović <milos.jov.rs@gmail.com>
2023-08-24 09:49:47 +0200

    chore: Prettier

--- frameworks/react/post.jsx
+++ frameworks/react/post.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
 export default function Post(props) {
-	return (
-		<article>
-			<Title>{props.title}</Title>
+  return (
+    <article>
+      <Title>{props.title}</Title>
 
-			<Content>{props.children}</Content>
+      <Content>{props.children}</Content>
 
-			<Footer>Published on {props.date}</Footer>
-		</article>
-	);
+      <Footer>Published on {props.date}</Footer>
+    </article>
+  )
 }
 
 let Title = styled.h1`
-	margin-bottom: 24px;
-`;
+  margin-bottom: 24px;
+`
 
 let Content = styled.div`
-	margin-bottom: 24px;
+  margin-bottom: 24px;
 
-	p:not(:last-child) {
-		margin-bottom: 8px;
-	}
-`;
+  p:not(:last-child) {
+    margin-bottom: 8px;
+  }
+`
 
 let Footer = styled.footer`
-	border-top: 1px solid #000;
-	padding-top: 24px;
-`;
+  border-top: 1px solid #000;
+  padding-top: 24px;
+`
